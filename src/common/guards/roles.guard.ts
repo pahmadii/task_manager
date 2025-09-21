ae3e988110fa47f65ff75b832a373da87c817ede
Parisa <fairyahmadi71@gmail.com>
2025-09-21 22:44:38 +0330

    create common folder added (guards,decorators,,interceptors)

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,36 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from '../../auth/entities/user.entity';
+
+interface RequestUser {
+  id: number;
+  role: UserRole;
+}
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<UserRole[]>('roles', context.getHandler());
+    if (!roles) return true;
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const request = context.switchToHttp().getRequest();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const user = request.user as RequestUser;
+
+    if (!user) throw new UnauthorizedException('User not authenticated');
+    if (!roles.includes(user.role)) {
+      throw new ForbiddenException('Access denied: insufficient role');
+    }
+
+    return true;
+  }
+}
