8e0f7bee88c4b2a9bb65142f022abdf7ee2251d1
Parisa <fairyahmadi71@gmail.com>
2025-09-23 09:28:56 +0330

    chore(profile): configure profile module and import dependencies

---
+++ src/profile/profile.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProfileService } from './profile.service';
+import { ProfileController } from './profile.controller';
+import { User } from '../auth/entities/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [ProfileController],
+  providers: [ProfileService],
+})
+export class ProfileModule {}
