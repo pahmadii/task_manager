d4e9d2421a16bd530803147a4ced200a0c645888
Parisa <fairyahmadi71@gmail.com>
2025-09-23 09:29:32 +0330

    feat(profile): create UpdateProfileDto with validation rules

---
+++ src/profile/dto/update-profile.dto.ts
@@ -0,0 +1,15 @@
+import { IsOptional, IsEmail, IsString } from 'class-validator';
+
+export class UpdateProfileDto {
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  fullName?: string;
+}
