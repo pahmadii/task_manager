c7662937a22f94961fd94c7db47718993deed954
Parisa <fairyahmadi71@gmail.com>
2025-10-04 18:34:44 +0330

    fix(migrations): adjust initial and bio migrations for role integration

--- migrations/1758695526491-InitialMigration.ts
+++ migrations/1758695526491-InitialMigration.ts
@@ -1,11 +1,8 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class InitialMigration1758695526491 implements MigrationInterface {
+  public async up(_queryRunner: QueryRunner): Promise<void> {}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(_queryRunner: QueryRunner): Promise<void> {}
 }
--- migrations/1758698933746-AddUserBio.ts
+++ migrations/1758698933746-AddUserBio.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddUserBio1758698933746 implements MigrationInterface {
-    name = 'AddUserBio1758698933746'
+  name = 'AddUserBio1758698933746';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "bio" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" ADD "bio" character varying`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "bio"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "bio"`);
+  }
 }
